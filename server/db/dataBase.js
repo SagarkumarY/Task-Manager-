aa682319ea5945e5c22fdbc6fa4b8852f6431ba5
Sagar <sagaryadav47232@gmail.com>
2023-11-09 21:23:47 +0530

    add .env file

--- server/db/dataBase.js
+++ server/db/dataBase.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
-const mongooseUrl = "mongodb://127.0.0.1:27017/TodoContextPro"; // Specify the database name
+// const mongooseUrl = "mongodb://127.0.0.1:27017/TodoContextPro"; // Specify the database name
+require('dotenv').config(); // Load environment variables from .env file
 
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(mongooseUrl, {
+        await mongoose.connect(process.env.DB_URI, {
             // useNewUrlParser: true,
             // useUnifiedTopology: true,
 
