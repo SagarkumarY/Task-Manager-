7997bc6bc56914f97c495deffd8b65dff000f994
Sagar <sagaryadav47232@gmail.com>
2023-11-10 21:46:06 +0530

    complete login and signup add context and modify

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,27 +1,56 @@
-import React from "react";
-
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "./context/Authentication/AuthContext";
 function Login() {
+  const navigate = useNavigate();
+  const authContext = useContext(AuthContext);
+  const { login } = authContext;
+  const [credentials, setCredentails] = useState({ email: "", password: "" });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    login(credentials, navigate);
+  };
+  const onChange = (e) => {
+    setCredentails({ ...credentials, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="container ">
       <h1 className="text-center mt-2">Login</h1>
-      <form className="login_form">
+      <form className="login_form" onSubmit={handleSubmit}>
         <div className="form-group">
-          <label htmlFor="exampleInputEmail1">Email address</label>
+          <label htmlFor="email">Email address</label>
           <input
+            // value={credentials.email}
+            // type="email"
+            // name="email"
+            // onChange={onChange}
+            // className="form-control"
+            // id="exampleInputEmail1"
+            // aria-describedby="emailHelp"
+            // placeholder="Enter email"
             type="email"
+            value={credentials.email}
+            onChange={onChange}
+            name="email"
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            autoComplete="username" // Add this line
           />
         </div>
         <div className="form-group">
-          <label htmlFor="exampleInputPassword1">Password</label>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
+            value={credentials.password}
+            onChange={onChange}
+            name="password"
             className="form-control"
-            id="exampleInputPassword1"
+            id="password"
             placeholder="Password"
+            autoComplete="current-password"
           />
         </div>
         <div className="form-group form-check"></div>
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,10 +1,51 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+// import config from "./config/Config";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "./context/Authentication/AuthContext";
 
 export default function Signup() {
+  const navigate = useNavigate();
+  const authContext = useContext(AuthContext);
+  const { signup} = authContext;
+  const [credentials, setCredentails] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleSubmit =  (e) => {
+    e.preventDefault();
+    signup(credentials,navigate)
+    // const { name, email, password } = credentials;
+    // try {
+    //   const response = await fetch(`${config.authUrl}/createuser`, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({ name, email, password }),
+    //   });
+
+    //   const json = await response.json();
+    //   if (response.status === 200) {
+    //     // The server responded with success
+    //     localStorage.setItem("token", json.authToken);
+    //     navigate("/login"); // Redirect to the login page
+    //   } else {
+    //     // The server did not respond with success
+    //     alert("Invalid credentials");
+    //   }
+    // } catch (error) {
+    //   console.error("An error occurred:", error);
+    // }
+  };
+
+  const onChange = (e) => {
+    setCredentails({ ...credentials, [e.target.name]: e.target.value });
+  };
   return (
     <div className="container">
       <h1 className="text-center mt-2 ">Signup</h1>
-      <form className="signup_form">
+      <form className="signup_form" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="name">Name </label>
           <input
@@ -14,6 +55,7 @@ export default function Signup() {
             name="name"
             aria-describedby="nameHelp"
             placeholder="Enter name"
+            onChange={onChange}
           />
         </div>
 
@@ -26,6 +68,8 @@ export default function Signup() {
             name="email"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            onChange={onChange}
+            minLength={6}
           />
         </div>
 
@@ -37,11 +81,14 @@ export default function Signup() {
             id="password"
             placeholder="Password"
             name="password"
+            onChange={onChange}
+            required
+            minLength={6}
           />
         </div>
 
         <button type="submit" className="btn mt-2 btn-primary">
-          Submit
+          Signup
         </button>
       </form>
     </div>
